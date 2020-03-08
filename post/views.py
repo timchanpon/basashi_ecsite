82ec08595ac1dc4cdcf24be36d7ddfeecb5fe41e
timshaw9 <tchad.by223@gmail.com>
2020-03-09 01:11:57 +0900

    仮トップページや、base.htmlなどを作成

--- post/views.py
+++ post/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+from .models import Post
+
+
+class IndexView(generic.ListView):
+    model = Post
+    template_name = 'index.html'
+
+    def get_queryset(self):
+        return Post.objects.order_by('-created_at')
