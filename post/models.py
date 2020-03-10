d9301bd0e415b59cd4a3500a2d81a5f9f3873512
timshaw9 <tchad.by223@gmail.com>
2020-03-10 14:55:40 +0900

    postモデルの商品写真フィールドのデフォルトファイルを設定

--- post/models.py
+++ post/models.py
@@ -6,7 +6,7 @@ class Post(models.Model):
 
     part_name = models.CharField(verbose_name='部位名', max_length=20)
     details = models.TextField(verbose_name='詳細', max_length=200, blank=True, null=True)
-    image = models.ImageField(verbose_name='商品画像', blank=True, null=True)
+    image = models.ImageField(verbose_name='商品画像', default='basashi_sample.jpg')
     num_grams = models.PositiveIntegerField(verbose_name='グラム数', default=100)
     purchase_amt = models.PositiveIntegerField(verbose_name='仕入れ量', default=1)
     stock_amt = models.PositiveIntegerField(verbose_name='在庫数', editable=False)
