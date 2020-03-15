e6798eb4ef488f5e6f39ce46f1726a06e9323972
timshaw9 <tchad.by223@gmail.com>
2020-03-15 18:25:16 +0900

    買い物かごモデルを作成

--- users/models.py
+++ users/models.py
@@ -1,4 +1,7 @@
+from django.db import models
 from django.contrib.auth.models import AbstractUser
+
+from posts.models import Post
 
 
 class CustomUser(AbstractUser):
@@ -6,3 +9,14 @@ class CustomUser(AbstractUser):
 
     class Meta:
         verbose_name_plural = '拡張ユーザーモデル'
+
+
+class ShoppingCart(models.Model):
+    """買い物かごモデル"""
+
+    user = models.ForeignKey(CustomUser, verbose_name='ユーザー', on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, verbose_name='ポスト', on_delete=models.CASCADE)
+    order_amt = models.PositiveIntegerField(verbose_name='購入数', editable=False)
+
+    class Meta:
+        verbose_name_plural = '買い物かごモデル'
