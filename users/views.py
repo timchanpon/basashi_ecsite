6981580795f23ba9c5c54fc41bf32fb1800641ad
timshaw9 <tchad.by223@gmail.com>
2020-03-15 20:47:27 +0900

    前コミット>>> ページ遷移エラー解消、次コミット>>> フォームcss要変更

--- users/views.py
+++ users/views.py
@@ -1,16 +1,17 @@
 from django.views import generic
-from django.urls import reverse_lazy
+from django.shortcuts import redirect
+from django.urls import reverse
 
 from posts.models import Post
 from .models import ShoppingCart
 
 
 class AddToShoppingCartView(generic.View):
-    def post(self, request):
+    def post(self, request, **kwargs):
         user = self.request.user
-        post = Post.objects.get(pk=self.kwargs['pk'])
+        post = Post.objects.get(pk=kwargs['pk'])
         order_amt = request.POST['order_amt']
         tmp = ShoppingCart(user=user, post=post, order_amt=order_amt)
         tmp.save()
 
-        return reverse_lazy('posts:post_list')
+        return redirect(reverse('posts:post_list'))
