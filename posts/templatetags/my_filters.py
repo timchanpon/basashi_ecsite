f633e7fc5ca5c1992b2748811dfbdbb4f93c4f96
timshaw9 <tchad.by223@gmail.com>
2020-03-16 13:09:44 +0900

    前コミット>>>参考ページURL追記
    https://qiita.com/nk-aonin/items/9cffecd8065caea5b512　「base.htmlにmodelを表示する方法」
    https://djangoproject.jp/doc/ja/1.0/howto/custom-template-tags.html　「自作フィルターの作成方法」
    https://docs.djangoproject.com/ja/1.11/ref/templates/builtins/#with　「django公式・with文の使い方」

--- posts/templatetags/my_filters.py
+++ posts/templatetags/my_filters.py
@@ -5,7 +5,7 @@ from users.models import CustomUser, ShoppingCart
 register = template.Library()
 
 
-@register.filter  # 関数名をフィルターの名前にするよう設定
+@register.filter
 def get_shopping_cart(username):
     user = CustomUser.objects.get(username=username)
     tmp = ShoppingCart.objects.filter(user=user)
