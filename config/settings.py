2b3ed7647197b21ffb0835ed2e78f9578887b62a
timshaw9 <tchad.by223@gmail.com>
2020-03-20 00:34:00 +0900

    CommonAppを作成し、各app共通のファイルbase.htmlとmy_filtersを移行

--- config/settings.py
+++ config/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'common.apps.CommonConfig',
     'posts.apps.PostsConfig',
     'users.apps.UsersConfig',
 
