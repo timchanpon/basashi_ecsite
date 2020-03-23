802edd399f0e1066bb12b68c824f28ec79cb8a4e
timshaw9 <tchad.by223@gmail.com>
2020-03-24 05:05:32 +0900

    拡張ユーザーモデルに作成した住所関係のモデル＆フィールドを住所アプリへ全て移行

--- users/admin.py
+++ users/admin.py
@@ -1,11 +1,7 @@
 from django.contrib import admin
 
-from .models import Prefecture, City, Chome, CustomUser, ShoppingCart
+from .models import CustomUser, ShoppingCart
 
 
 admin.site.register(CustomUser)
 admin.site.register(ShoppingCart)
-
-admin.site.register(Prefecture)
-admin.site.register(City)
-admin.site.register(Chome)
--- users/models.py
+++ users/models.py
@@ -4,60 +4,8 @@ from django.contrib.auth.models import AbstractUser
 from posts.models import Post
 
 
-class Prefecture(models.Model):
-    """都道府県モデル"""
-
-    pref_name = models.CharField(verbose_name='都道府県名', max_length=5)
-
-    class Meta:
-        verbose_name_plural = '都道府県モデル'
-
-    def __str__(self):
-        return self.pref_name
-
-
-class City(models.Model):
-    """市区町村モデル"""
-
-    pref = models.ForeignKey(Prefecture, verbose_name='都道府県', on_delete=models.PROTECT)
-    city_name = models.CharField(verbose_name='市区町村名', max_length=10)
-
-    class Meta:
-        verbose_name_plural = '市区町村モデル'
-
-    def __str__(self):
-        pref_name = self.pref.pref_name
-        city_name = self.city_name
-
-        return pref_name + city_name
-
-
-class Chome(models.Model):
-    """丁目モデル"""
-
-    city = models.ForeignKey(City, verbose_name='市区町村', on_delete=models.PROTECT)
-    chome = models.CharField(verbose_name='丁目', max_length=10)
-
-    class Meta:
-        verbose_name_plural = '丁目モデル'
-
-    def __str__(self):
-        pref_name = self.city.pref.pref_name
-        city_name = self.city.city_name
-        chome = self.chome
-
-        return pref_name + city_name + chome
-
-
 class CustomUser(AbstractUser):
     """拡張ユーザーモデル"""
-
-    pref = models.ForeignKey(Prefecture, verbose_name='都道府県', on_delete=models.SET_NULL, blank=True, null=True)
-    city = models.ForeignKey(City, verbose_name='市区町村', on_delete=models.SET_NULL, blank=True, null=True)
-    chome = models.ForeignKey(Chome, verbose_name='丁目', on_delete=models.SET_NULL, blank=True, null=True)
-    banchi_go = models.CharField(verbose_name='番地・号', max_length=10, blank=True, null=True)
-    building = models.CharField(verbose_name='建物', max_length=20, blank=True, null=True)
-    address = models.CharField(verbose_name='住所', max_length=55, editable=False, blank=True, null=True)
 
     class Meta:
         verbose_name_plural = '拡張ユーザーモデル'
