b4d81f9de106a3f3893909fd60a159755580a991
timshaw9 <tchad.by223@gmail.com>
2020-03-24 05:21:34 +0900

    拡張ユーザーモデルにユーザーアイコンフィールドを追加

--- users/models.py
+++ users/models.py
@@ -7,6 +7,8 @@ from posts.models import Post
 class CustomUser(AbstractUser):
     """拡張ユーザーモデル"""
 
+    user_icon = models.ImageField(verbose_name='ユーザーアイコン', default='default_user_icon.jpg')
+
     class Meta:
         verbose_name_plural = '拡張ユーザーモデル'
 
