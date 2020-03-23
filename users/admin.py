1eb5266a4f49278adb3ad0bbe4ed91ccfd8b59c6
timshaw9 <tchad.by223@gmail.com>
2020-03-24 04:24:03 +0900

    都道府県＆市区町村＆丁目モデルを作成し、拡張ユーザーモデルに住所フィールドを追加

--- users/admin.py
+++ users/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 
-from .models import CustomUser, ShoppingCart
+from .models import Prefecture, City, Chome, CustomUser, ShoppingCart
 
 
 admin.site.register(CustomUser)
 admin.site.register(ShoppingCart)
+
+admin.site.register(Prefecture)
+admin.site.register(City)
+admin.site.register(Chome)
