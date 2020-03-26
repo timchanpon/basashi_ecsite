501a1e3cfaf8a6baf6cea3b56cf6757daa570fdd
timchanpon <62697984+timchanpon@users.noreply.github.com>
2020-03-27 00:39:17 +0900

    ユーザーモデルに注文回数、注文モデルに注文番号フィールドを追加

--- users/models.py
+++ users/models.py
@@ -8,6 +8,7 @@ class CustomUser(AbstractUser):
     """拡張ユーザーモデル"""
 
     user_icon = models.ImageField(verbose_name='ユーザーアイコン', default='default_user_icon.jpg')
+    order_count = models.PositiveIntegerField(verbose_name='注文回数', default=0, editable=False)
 
     class Meta:
         verbose_name_plural = '拡張ユーザーモデル'
