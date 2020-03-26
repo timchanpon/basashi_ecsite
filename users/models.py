b66bf9ac05164818efb350752cb970c4c8272a5c
timchanpon <62697984+timchanpon@users.noreply.github.com>
2020-03-27 01:35:29 +0900

    買い物かごモデルを消去

--- users/models.py
+++ users/models.py
@@ -12,24 +12,3 @@ class CustomUser(AbstractUser):
 
     class Meta:
         verbose_name_plural = '拡張ユーザーモデル'
-
-
-class ShoppingCart(models.Model):
-    """買い物かごモデル"""
-
-    user = models.ForeignKey(CustomUser, verbose_name='ユーザー', on_delete=models.CASCADE)
-    post = models.ForeignKey(Post, verbose_name='ポスト', on_delete=models.CASCADE)
-    order_amt = models.PositiveIntegerField(verbose_name='購入数', default=0)
-
-    created_at = models.DateTimeField(verbose_name='作成日時', auto_now_add=True)
-    updated_at = models.DateTimeField(verbose_name='更新日時', auto_now=True)
-
-    class Meta:
-        verbose_name_plural = '買い物かごモデル'
-
-    def __str__(self):
-        user = self.user
-        post = self.post
-        order_amt = self.order_amt
-
-        return f'{user} {post} {order_amt}個'
