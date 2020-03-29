b8235949ce729f1ef708ef8ce8a6e6d7a83fb70b
timchanpon <62697984+timchanpon@users.noreply.github.com>
2020-03-29 12:16:40 +0900

    OrderItemモデルのstatusフィールドをin_cart(boolean)に変更

--- orders/models.py
+++ orders/models.py
@@ -10,7 +10,7 @@ class OrderItem(models.Model):
     user = models.ForeignKey(CustomUser, verbose_name='ユーザー', on_delete=models.CASCADE)
     post = models.ForeignKey(Post, verbose_name='ポスト', on_delete=models.CASCADE)
     order_amt = models.PositiveIntegerField(verbose_name='購入数', default=0)
-    status = models.CharField(verbose_name='ステータス', max_length=10, default='in_cart', editable=False)
+    in_cart = models.BooleanField(verbose_name='買い物かごの中か否か', default=True, editable=False)
 
     created_at = models.DateTimeField(verbose_name='作成日時', auto_now_add=True)
     updated_at = models.DateTimeField(verbose_name='更新日時', auto_now=True)
@@ -22,6 +22,9 @@ class OrderItem(models.Model):
         user = self.user
         post = self.post
         order_amt = self.order_amt
-        status = self.status
+        if self.in_cart:
+            status = 'in_cart'
+        else:
+            status = ''
 
         return f'{user} {post} {order_amt}個 ({status})'
