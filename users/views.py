08a178aa10799cad62028d00755810d18cc71e31
timchanpon <62697984+timchanpon@users.noreply.github.com>
2020-03-30 22:18:07 +0900

    (add) ユーザー情報ページに注文表示を追加

--- users/views.py
+++ users/views.py
@@ -2,6 +2,7 @@ from django.views import generic
 from django.contrib.auth.mixins import UserPassesTestMixin
 
 from addresses.models import Address
+from orders.models import Order
 from .models import CustomUser
 
 
@@ -17,7 +18,11 @@ class UserDetailView(UserPassesTestMixin, generic.DetailView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         user = self.request.user
+
         addresses = Address.objects.filter(user=user)
+        orders = Order.objects.filter(user=user, has_delivered=False, has_cancelled=False).order_by('-ordered_at')
+
         context['addresses'] = addresses
+        context['orders'] = orders
 
         return context
