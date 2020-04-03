986be2881ea0c32dcc545b3337f373bdf1c711a5
timchanpon <62697984+timchanpon@users.noreply.github.com>
2020-04-03 22:23:09 +0900

    (add) 本番用設定ファイルにロギング設定を追加、gitignoreにログ出力先ディレクトリを追加

--- config/settings/production.py
+++ config/settings/production.py
@@ -11,7 +11,68 @@ ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOSTS')]
 STATIC_ROOT = '/usr/share/nginx/html/static'
 MEDIA_ROOT = '/usr/share/nginx/html/media'
 
+
 # Amazon SES関連設定
 AWS_SES_ACCESS_KEY_ID = os.environ.get('AWS_SES_ACCESS_KEY_ID')
 AWS_SES_SECRET_ACCESS_KEY = os.environ.get('AWS_SES_SECRET_ACCESS_KEY')
 EMAIL_BACKEND = 'django_ses.SESBackend'
+
+
+# ロギング設定
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+
+    # ロガーの設定
+    'loggers': {
+        'django': {
+            'handlers': ['file'],
+            'level': 'INFO',
+        },
+        'addresses': {
+            'handlers': ['file'],
+            'level': 'INFO',
+        },
+        'common': {
+            'handlers': ['file'],
+            'level': 'INFO',
+        },
+        'orders': {
+            'handlers': ['file'],
+            'level': 'INFO',
+        },
+        'posts': {
+            'handlers': ['file'],
+            'level': 'INFO',
+        },
+        'users': {
+            'handlers': ['file'],
+            'level': 'INFO',
+        },
+    },
+
+    # ハンドラの設定
+    'handlers': {
+        'file': {
+            'level': 'INFO',
+            'class': 'logging.handlers.TimedRotatingFileHandler',
+            'filename': os.path.join(BASE_DIR, 'logs/django.log'),
+            'formatter': 'prod',
+            'when': 'D',  # ログローテーション(新しいファイルへの切り替え)間隔の単位(D=日)
+            'interval': 1,  # ログローテーション間隔(1日単位)
+            'backupCount': 7,  # 保存しておくログファイル数
+        },
+    },
+
+    # フォーマッタの設定
+    'formatters': {
+        'prod': {
+            'format': '\t'.join([
+                '%(asctime)s',
+                '[%(levelname)s]',
+                '%(pathname)s(Line:%(lineno)d)',
+                '%(message)s'
+            ])
+        },
+    }
+}
